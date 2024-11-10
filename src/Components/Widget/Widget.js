18c9a7c3984cb7542ce1f456a0c2fb4aab63e6c0
Ramsha42001 <90575580+Ramsha42001@users.noreply.github.com>
2024-11-10 16:32:42 +0530

    updated url

--- src/Components/Widget/Widget.js
+++ src/Components/Widget/Widget.js
@@ -22,9 +22,9 @@ const Widget = () => {
 
             try {
                 const messageQuery = encodeURIComponent(newMessage);
-                const botId = "bot1"; 
-                const response = await axios.get(`https://rapidbot-2327227512.us-central1.run.app/api/chat?message=${messageQuery}&botId=${botId}`);
-
+                const botId = 11; 
+                const response = await axios.get(`https://rapidbot-2327227512.us-central1.run.app/api/chat?message=${messageQuery}&bot_id=${botId}`);
+                console.log(response);
                 const botMessage = { sender: "bot", text: response.data.text || "I'm here to assist you!" };
                 setMessages(prevMessages => [...prevMessages, botMessage]);
 
